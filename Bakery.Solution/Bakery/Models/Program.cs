c32338e74878a6e936d6a8f90ff2cf2db696933c
William Rodriguez <william@rodriguez1.com>
2022-05-06 15:21:03 -0700

    Add console interaction for user to buy from bakery

--- Bakery.Solution/Bakery/Models/Program.cs
+++ Bakery.Solution/Bakery/Models/Program.cs
@@ -6,6 +6,23 @@ namespace Bakery
 {
   public class Program
   {
-    public static void Main() {}
+    public static void Main() 
+    {
+      Bread bread = new Bread();
+      Pastry pastry = new Pastry();
+
+      Console.WriteLine("Welcome to the bakery!");
+      int breadQuantity = int.Parse(GetInput("How much bread would you like? "));
+      int pastryQuantity = int.Parse(GetInput("How many pastries would you like? "));
+
+      Console.WriteLine($"That will cost ${bread.Buy(breadQuantity) + pastry.Buy(pastryQuantity)}");
+      Console.WriteLine("Come again soon!");
+    }
+
+    private static string GetInput(string prompt)
+    {
+      Console.WriteLine(prompt);
+      return Console.ReadLine();
+    }
   }
 }
